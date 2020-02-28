03ca336eead077bb48793089e3e077c079838c21
Adil Khan <akhan@kpd.be>
2020-02-28 11:35:35 +0100

    added emotion and new react build

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <form>
+      <p>This is a form</p>
+      <input></input>
+      <input></input>
+      <input></input>
+      <input></input>
+    </form>
+  );
+}
+
+export default App;
